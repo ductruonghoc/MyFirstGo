0601f0cab22e85b1e00bbc5861d8864f1a84991f
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2024-09-22 11:30:37 +0700

    1.0.1
    Preproduct

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import "fmt"
+import "main/model"
 
 func main() {
-	fmt.Println("Hello, World!!!");
+	fmt.Println(model.Hi());
 }
---
+++ model/model.go
@@ -0,0 +1,6 @@
+package model
+
+func Hi() string{
+	
+	return "Hello, World!!!";
+}
